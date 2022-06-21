4a482f3a312b9e1c6fb3f17e587fb18aadaf970d
Osama Youssef <osamayoussef996@gmail.com>
2022-06-22 00:43:56 +0200

    Assignment_3

--- Assignment_3_C_Function/Assignment_3_Example_3/main.c
+++ Assignment_3_C_Function/Assignment_3_Example_3/main.c
@@ -2,7 +2,25 @@
  * main.c
  *
  *  Created on: Jun 17, 2022
- *      Author: Dell
+ *      Author: Osama Youssef 
  */
+#include"stdio.h"
+void reverse();
+int main()
+{
+	printf("Enter a sentence :");    
+	reverse();   
 
+	return 0;
+}
+void reverse()
+{
+	char c;
+	scanf("%c",&c);
+	if(c != '\n')
+	{
+		reverse();
+		printf("%c",c);
+	}
+}
 
